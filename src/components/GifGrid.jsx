30b5e73333f8c14fc84dc27e15fe5371722b4c60
Rodolfo Echenique <echeniquer@gmail.com>
2023-02-20 12:42:25 -0300

    Seccion 6 - video 87 Listo

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from "react"
-
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async() => {
-        const newImages = await getGifs(category);
-        setImages(newImages);
-    }
-
-    useEffect(() => {
-        getImages();
-    }, []);
+    const { images, isLoading } = useFetchGifs(category);
 
     return (
         <>
