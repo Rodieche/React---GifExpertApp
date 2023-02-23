a44a9aee0ca0a853d320420eb3c69df635bf4c6e
Rodolfo Echenique <echeniquer@gmail.com>
2023-02-22 21:40:17 -0300

    Pruebas unitarias en GifItem

---
+++ tests/components/GifItem.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components/GifItem";
+
+describe('Pruebas en <GifItem />', () => {
+    
+    const title = 'Demon Slayer';
+    const url = 'https://demon-slayer.com/tanjiro.jpg';
+
+    test('debe hacer match con snapshot', () => {
+        const { container } = render(<GifItem title={ title } url={ url } />)
+        expect(container).toMatchSnapshot();
+    })
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={ title } url={ url } />);
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title)
+    })
+
+    test('debe de mostrar el titulo en el componente', () => {
+        render(<GifItem title={ title } url={ url } />);
+        expect(screen.getByText(title)).toBeTruthy();
+    })
+
+});
