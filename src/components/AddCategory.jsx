fa3d5fc387625e64cd8442d6d821a6e70e34d992
Rodolfo Echenique <echeniquer@gmail.com>
2023-04-12 01:46:08 -0300

    Pruebas unitarias en AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
   
@@ -18,7 +19,7 @@ export const AddCategory = ({ onNewCategory }) => {
 
     return (
     <>
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form" >
             <input 
                 type="text"
                 placeholder="Buscar GIFs"
@@ -28,4 +29,9 @@ export const AddCategory = ({ onNewCategory }) => {
         </form>
     </>
   )
+}
+
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
